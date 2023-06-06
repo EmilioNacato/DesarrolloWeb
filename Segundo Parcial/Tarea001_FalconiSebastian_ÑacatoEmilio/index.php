6c73b97a3ed72661f9160d07c07403e7c3e96eb0
EmilioNacato <ejnacato@espe.edu.ec>
2023-06-05 20:04:21 -0500

    Eliminacion

--- Segundo Parcial/Tarea001_FalconiSebastian_ÑacatoEmilio/index.php
+++
@@ -1,6 +0,0 @@
-<?php include("db.php"); ?>
-<?php include("includes/header.php"); ?>
-
-<?php include("php/index.php"); ?>
-
-<?php include("includes/footer.php"); ?>
--- Segundo Parcial/Tarea001_FalconiSebastian_ÑacatoEmilio/php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Cerrar la sesión
-
-header("Location: ../index.php"); // Redirigir al usuario al formulario de inicio de sesión
-exit();
-?>
